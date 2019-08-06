97a6c9542c61c16243473c1de82cfe549aa3180d
m-conti <mc.heux@gmail.com>
2019-08-06 17:44:14 +0200

    package front

--- backend/dummy_backend/views.py
+++ backend/dummy_backend/views.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .serializers import DummySerializer
 from .models import Dummy
 
+from rest_framework.response import Response
+
 
 class DummyView(ModelViewSet):
-    serializer_class = DummySerializer
-    queryset = Dummy.objects.all()
+	authentication_classes = [TokenAuthentication]
+	permission_classes = [IsAuthenticated]
+
+
+	serializer_class = DummySerializer
+	queryset = Dummy.objects.all()
