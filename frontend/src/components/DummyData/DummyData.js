cfc97dfd142d29bcde763eaf4107c299d001d90c
Julien BOURSALY <jboursal@e1r2p13.42.fr>
2019-08-06 16:31:34 +0200

    data fetched to the screen

---
+++ frontend/src/components/DummyData/DummyData.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DummyData = (props) => {
+	return (
+		<div>
+			<p>{ props.name }</p>
+			<p>{ props.data }</p>
+		</div>
+	);
+}
+
+export default DummyData
---
+++ frontend/src/components/FullData/FullData.js
@@ -0,0 +1,55 @@
+import React, { Component, useState, useEffect } from 'react';
+import axios from 'axios';
+
+import DummyData from './../DummyData/DummyData';
+
+
+const initialState = {
+	data : [],
+}
+
+const FullData = () => {
+	
+
+	const [state, setState] = useState(initialState);
+
+	useEffect(() => {
+			axios
+				.get('http://localhost:8000/api/dummy/')
+				.then(result => setState({ ...state, data: result.data }))
+				.catch(error => console.log(error));
+		}, []);
+
+	const data = state.data.map((dummyData, key) => (
+			<DummyData name={ dummyData.name } data={ dummyData.data} key={ key }/>
+		));
+	
+	return (
+		<div>{ data }</div>
+	);
+};
+
+//class FullData extends Component {
+//	state = initialState;
+//
+//	componentDidMount () {
+//		axios
+//			.get('http://localhost:8000/api/dummy/')
+//			.then(result => this.setState({ data:result.data }))
+//			.catch(error => console.log(error));
+//	}
+//
+//	render() {
+//		const data = this.state.data.map((dummyData, key) => {
+//			return (
+//				<DummyData name={ dummyData.name } data={ dummyData.data} key={ key }/>
+//			);
+//		});
+//
+//		return (
+//			<div>{ data }</div>
+//		);
+//	}
+//}
+
+export default FullData
